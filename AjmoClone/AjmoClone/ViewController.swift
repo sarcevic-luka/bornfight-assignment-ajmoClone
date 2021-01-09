b19fdf98f7144ffac3432e4a59745d013189f37c
Luka Sarcevic <luka.sarcevic@analyticom.de>
2021-01-09 16:20:25 +0100

    * Added base project files.

---
+++ AjmoClone/AjmoClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AjmoClone
+//
+//  Created by Luka Šarčević on 09.01.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
